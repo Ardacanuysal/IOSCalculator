31c32ad2cacaa22bc8030f95a6ba2a03cf328291
Arda Can Uysal <ardacanuysal03@gmail.com>
2023-01-25 19:35:52 +0300

    Update ContentView.swift

--- Calculator.SwiftUI/ContentView.swift
+++ Calculator.SwiftUI/ContentView.swift
@@ -8,19 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons  = [
+    
+    ["7","8","9","X"],
+    ["4","5","6","-"],
+    ["1","2","3","+"],
+    ["0",".",".","="],
+    
+    ]
+    
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .bottom){
+            Color.black.edgesIgnoringSafeArea(.all)
+
+            VStack {
+                
+        
+   
+                
+                Text("000").foregroundColor(.white)
+                    .font(.system(size: 64))
+            
+                
+            }
+            
+            
+                ForEach(buttons, id: \.self){ row in
+                    HStack{
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.white)
+                        }
+                        
+                    }
+                }
+              
+                
+             
+            }
         }
-        .padding()
+
+        
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
