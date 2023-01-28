cd6efbb4f46cefc64b4263e8b01de57f5a643aa8
Arda Can Uysal <ardacanuysal03@gmail.com>
2023-01-28 17:47:37 +0300

    array

--- Calculator.SwiftUI/ContentView.swift
+++ Calculator.SwiftUI/ContentView.swift
@@ -10,54 +10,50 @@ import SwiftUI
 struct ContentView: View {
     
     let buttons  = [
-    
-    ["7","8","9","X"],
-    ["4","5","6","-"],
-    ["1","2","3","+"],
-    ["0",".",".","="],
-    
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".",".","="],
     ]
     
-
     var body: some View {
+        
         ZStack(alignment: .bottom){
             Color.black.edgesIgnoringSafeArea(.all)
-
+            
             VStack {
                 
-        
-   
+                HStack{
+                    Spacer()
+                    Text("0").foregroundColor(.white)
+                        .font(.system(size:72))
+                    
+                }
                 
-                Text("000").foregroundColor(.white)
-                    .font(.system(size: 64))
-            
-                
-            }
-            
-            
-                ForEach(buttons, id: \.self){ row in
-                    HStack{
-                        ForEach(row, id: \.self) { button in
+                ForEach(buttons,id: \.self){ row in
+                    HStack {
+                        ForEach(row,id: \.self){ button in
                             Text(button)
                                 .font(.system(size: 32))
-                                .frame(width: 80, height: 80)
+                                .frame(width: 80, height:  80)
                                 .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(40)
                         }
-                        
                     }
                 }
-              
-                
-             
-            }
+            }.padding(.bottom)
         }
-
-        
     }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
     }
-}
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
 
